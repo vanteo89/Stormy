f39a90a20e82df4b2e31c5e1b7de575049a5efb3
vanteo89 <juanmjuan89@gmail.com>
2015-08-10 10:59:19 +0700

    gfhhg

--- app/src/main/java/com/example/vanteo89/stormy/CurrentWeather.java
+++ app/src/main/java/com/example/vanteo89/stormy/CurrentWeather.java
@@ -79,8 +79,8 @@ public class CurrentWeather {
         this.mTime = mTime;
     }
 
-    public double getmTemperature() {
-        return mTemperature;
+    public int getmTemperature() {
+        return (int) Math.round(mTemperature);
     }
 
     public void setmTemperature(double mTemperature) {
@@ -95,8 +95,9 @@ public class CurrentWeather {
         this.mHumidity = mHumidity;
     }
 
-    public double getmPrecipChance() {
-        return mPrecipChance;
+    public int getmPrecipChance() {
+        double PrecipChance=this.mPrecipChance*100;
+        return (int) Math.round(PrecipChance);
     }
 
     public void setmPrecipChance(double mPrecipChance) {
--- app/src/main/java/com/example/vanteo89/stormy/MainActivity.java
+++ app/src/main/java/com/example/vanteo89/stormy/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.vanteo89.stormy;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.ActionBarActivity;
@@ -10,6 +11,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,25 +36,49 @@ public class MainActivity extends ActionBarActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
 
     private CurrentWeather mCurrentWeather;
-    @Bind(R.id.tv_humidity) TextView tvHumidity;
-    @Bind(R.id.tv_humidity_value) TextView tvHumdityValue;
-    @Bind(R.id.tv_location)TextView tvLocation;
-    @Bind(R.id.tv_precip)TextView tvPrecip;
-    @Bind(R.id.tv_precip_value)TextView tvPrecipValue;
-    @Bind(R.id.tv_temperate)TextView tvTemperate;
-    @Bind(R.id.tv_time)TextView tvTime;
-    TextView txt;
+    @Bind(R.id.tv_humidity)
+    TextView tvHumidity;
+    @Bind(R.id.tv_humidity_value)
+    TextView tvHumdityValue;
+    @Bind(R.id.tvLocation)
+    TextView tvLocation;
+    @Bind(R.id.tv_precip)
+    TextView tvPrecip;
+    @Bind(R.id.tv_precip_value)
+    TextView tvPrecipValue;
+    @Bind(R.id.tv_temperate)
+    TextView tvTemperate;
+    @Bind(R.id.tv_time)
+    TextView tvTime;
+    @Bind(R.id.tvSummary)
+    TextView tvSummary;
+    @Bind(R.id.img_clound_night)
+    ImageView imgClound;
+    @Bind(R.id.imgRefesh)
+    ImageView imgRefesh;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        final double lattitude = 16.035621; //37.8267;
+        final double longitude = 108.244557;//-122.423;
+        imgRefesh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(),"fkjdsfkjdsh",Toast.LENGTH_LONG).show();
+                getForeCast(lattitude,longitude);
+            }
+        });
+        getForeCast(lattitude,longitude);
+    }
 
+    private void getForeCast(double latitude,double longitude) {
         String apiKey = "9b030f2c8e522e496e64930931e36233";
-        double lattitude =16.035621; //37.8267;
-        double longitude = 108.244557;//-122.423;
-        String foreCastUrl = "https://api.forecast.io/forecast/" + apiKey + "/" + lattitude + "," + longitude;
+
+        String foreCastUrl = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude + "," + longitude;
         if (isNetworkValiable()) {
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder().url(foreCastUrl).build();
@@ -71,6 +98,13 @@ public class MainActivity extends ActionBarActivity {
                         //Returns true if the code is in [200..300), which means the request was successfully received, understood, and accepted.
                         if (response.isSuccessful()) {
                             mCurrentWeather = getCurrentDetails(jsonData);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    updateDisplay();
+                                }
+                            });
+
                         } else {
                             alertUserAboutError();
                         }
@@ -88,10 +122,22 @@ public class MainActivity extends ActionBarActivity {
         Log.v(TAG, "Main UI Thread Runing");
     }
 
+    private void updateDisplay() {
+        tvTemperate.setText(mCurrentWeather.getmTemperature() + "");
+        tvTime.setText("At " + mCurrentWeather.getFormattedTime() + " will be at");
+        tvHumdityValue.setText(mCurrentWeather.getmHumidity() + "");
+        tvPrecipValue.setText(mCurrentWeather.getmPrecipChance() + " %");
+        tvSummary.setText(mCurrentWeather.getmSummary());
+        Drawable drawable = getResources().getDrawable(mCurrentWeather.getIconId());
+        imgClound.setImageDrawable(drawable);
+        tvLocation.setText(mCurrentWeather.getTimeZone());
+
+    }
+
     private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
         JSONObject foreCast = new JSONObject(jsonData);
         String timeZone = foreCast.getString("timezone");
-        Log.d(TAG,timeZone);
+        Log.d(TAG, timeZone);
         Log.i(TAG, "From Json :" + timeZone);
         JSONObject currently = foreCast.getJSONObject("currently");
 
@@ -102,8 +148,8 @@ public class MainActivity extends ActionBarActivity {
         currentWeather.setmPrecipChance(currently.getDouble("precipProbability"));
         currentWeather.setmSummary(currently.getString("summary"));
         currentWeather.setmTemperature(currently.getDouble("temperature"));
-       currentWeather.setTimeZone(timeZone);
-        Log.d(TAG,currentWeather.getFormattedTime());
+        currentWeather.setTimeZone(timeZone);
+        Log.d(TAG, currentWeather.getFormattedTime());
         return currentWeather;
     }
 
